856e627640ec39f725a77906716ca1076a14fa22
Laurent Brand <laurentbrand19@gmail.com>
2022-05-24 09:12:02 +0200

    added leaflet.js map

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <custom-map></custom-map>
+  </div>
+</template>
+
+<script>
+import CustomMap from './components/CustomMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CustomMap,
+  }
+}
+</script>
+
---
+++ frontend/src/components/CustomMap.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <l-map
+        :center="center"
+        :zoom="zoom"
+        class="map"
+        ref="map"
+        @update:center="centerUpdated"
+        @update:zoom="zoomUpdated"
+        >
+        <l-tile-layer
+        :url="url"
+        >
+
+        </l-tile-layer>
+        </l-map>
+    </div>
+</template>
+
+<script>
+import { LMap, LTileLayer } from 'vue2-leaflet';
+
+export default {
+    components: {
+        LMap,
+        LTileLayer
+    },
+    data () {
+        return {
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            center: [49.61430686205902, 6.128856270211184],
+            zoom: 12
+        }
+    },
+    methods: {
+        centerUpdated (center) {
+            this.center = center;
+        },
+        zoomUpdated (zoom) {
+            this.zoom = zoom;
+        }
+    }
+}
+</script>
+<style scoped>
+  .map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+</style>
